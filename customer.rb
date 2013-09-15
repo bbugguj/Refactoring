d49860846b0cb47444d37a497baa4076f3aa8dd5
bbugguj <bbugguj@Jaehue-ui-MacBook-Pro.local>
2013-09-16 00:46:34 +0900

    frequent_renter_points에도 적용

--- customer.rb
+++ customer.rb
@@ -13,18 +13,23 @@ class Customer
   def statement
   	frequent_renter_points = 0
   	result = "고객 #{@name}의 대여 기록\n"
-  	@rentals.each do |element|
-  	  frequent_renter_points += element.frequent_renter_points
+  	@rentals.each do |element|  	  
   	  # 이번 대여의 계산 결과를 표시
   	  result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
   	end
   	# 푸터 행 추가
   	result += "대여료는 #{total_charge}입니다.\n"
-  	result += "적립 포인트는 #{frequent_renter_points}입니다."
+  	result += "적립 포인트는 #{total_frequent_renter_points}입니다."
   	result
   end
 
+  private
+  
   def total_charge    
     @rentals.inject(0) { |sum, rental| sum + rental.charge  }    
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points  }
   end
  end
